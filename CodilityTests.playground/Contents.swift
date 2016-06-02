67a4bc1ab71bac32922baae549672471e05fcb27
Murali Korukonda <muralikrishna.korukonda@gmail.com>
2016-06-02 22:33:44 +0800

    Lesson 3 solution

--- CodilityTests.playground/Contents.swift
+++ CodilityTests.playground/Contents.swift
@@ -26,7 +26,7 @@ import UIKit
  expected worst-case time complexity is O(log(N));
  expected worst-case space complexity is O(1).
  */
-public func solution(N : Int) -> Int {
+public func solution1(N : Int) -> Int {
     let str = String(N, radix: 2)
     var maxGap = 0, gapCounter = 0, gapStarted = false
     for i in str.characters {
@@ -48,7 +48,7 @@ public func solution(N : Int) -> Int {
     
 }
 
-print(solution(1041))
+print(solution1(1041))
 
 
 //Lesson 2 - Arrays
@@ -72,7 +72,7 @@ print(solution(1041))
  each element of array A is an integer within the range [−1,000..1,000].
  In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
  */
-public func solution(inout A : [Int], _ K : Int) -> [Int] {
+public func solution2(inout A : [Int], _ K : Int) -> [Int] {
     guard A.count > 0 else {
         return []
     }
@@ -85,7 +85,7 @@ public func solution(inout A : [Int], _ K : Int) -> [Int] {
     return A
 }
 var A = [1,2,3,4,5]
-print(solution(&A, 2))
+print(solution2(&A, 2))
 
 //OddOccurencesInArray
 /*
@@ -124,7 +124,7 @@ print(solution(&A, 2))
  expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
  Elements of input arrays can be modified.
  */
-public func solution(inout A : [Int]) -> Int {
+public func solution2(inout A : [Int]) -> Int {
     var temp = 0
     for i in A {
         temp = temp ^ i
@@ -132,8 +132,153 @@ public func solution(inout A : [Int]) -> Int {
     return temp
 }
 
-print(solution(&A))
+print(solution2(&A))
 
 
+//Lesson 3 - Time Complexity
 
+//FrogJmp
+/*
+ A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to get to a position greater than or equal to Y. The small frog always jumps a fixed distance, D.
+ 
+ Count the minimal number of jumps that the small frog must perform to reach its target.
+ 
+ Write a function:
+ 
+ public func solution(X : Int, _ Y : Int, _ D : Int) -> Int
+ 
+ that, given three integers X, Y and D, returns the minimal number of jumps from position X to a position equal to or greater than Y.
+ 
+ For example, given:
+ 
+ X = 10
+ Y = 85
+ D = 30
+ the function should return 3, because the frog will be positioned as follows:
+ 
+ after the first jump, at position 10 + 30 = 40
+ after the second jump, at position 10 + 30 + 30 = 70
+ after the third jump, at position 10 + 30 + 30 + 30 = 100
+ Assume that:
+ 
+ X, Y and D are integers within the range [1..1,000,000,000];
+ X ≤ Y.
+ Complexity:
+ 
+ expected worst-case time complexity is O(1);
+ expected worst-case space complexity is O(1).
+ */
+
+public func  solution3(X : Int, _ Y : Int, _ D : Int) -> Int {
+    return Int(ceil((Double(Y)-Double(X))/Double(D)))
+}
+
+print(solution3(10, 86, 30))
+
+
+//TapeEquilibrium
+/*
+ A non-empty zero-indexed array A consisting of N integers is given. Array A represents numbers on a tape.
+ 
+ Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+ 
+ The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+ 
+ In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+ 
+ For example, consider array A such that:
+ 
+ A[0] = 3
+ A[1] = 1
+ A[2] = 2
+ A[3] = 4
+ A[4] = 3
+ We can split this tape in four places:
+ 
+ P = 1, difference = |3 − 10| = 7
+ P = 2, difference = |4 − 9| = 5
+ P = 3, difference = |6 − 7| = 1
+ P = 4, difference = |10 − 3| = 7
+ Write a function:
+ 
+ public func solution(inout A : [Int]) -> Int
+ 
+ that, given a non-empty zero-indexed array A of N integers, returns the minimal difference that can be achieved.
+ 
+ For example, given:
+ 
+ A[0] = 3
+ A[1] = 1
+ A[2] = 2
+ A[3] = 4
+ A[4] = 3
+ the function should return 1, as explained above.
+ 
+ Assume that:
+ 
+ N is an integer within the range [2..100,000];
+ each element of array A is an integer within the range [−1,000..1,000].
+ Complexity:
+ 
+ expected worst-case time complexity is O(N);
+ expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+ Elements of input arrays can be modified.
+ */
+
+public func solution3(inout A : [Int]) -> Int {
+    var head = A[0]
+    var tail = A[1..<A.count].reduce(0, combine: +)
+    var minDif = abs(head - tail)
+    
+    for index in 1..<A.count-1 {
+        head += A[index]
+        tail -= A[index]
+        if abs(head - tail) < minDif {
+            minDif = abs(head - tail)
+        }
+    }
+    
+    return minDif
+}
+print(solution3(&A))
+
+//PermMissingElem
+/*
+ A zero-indexed array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+ 
+ Your goal is to find that missing element.
+ 
+ Write a function:
+ 
+ public func solution(inout A : [Int]) -> Int
+ 
+ that, given a zero-indexed array A, returns the value of the missing element.
+ 
+ For example, given array A such that:
+ 
+ A[0] = 2
+ A[1] = 3
+ A[2] = 1
+ A[3] = 5
+ the function should return 4, as it is the missing element.
+ 
+ Assume that:
+ 
+ N is an integer within the range [0..100,000];
+ the elements of A are all distinct;
+ each element of array A is an integer within the range [1..(N + 1)].
+ Complexity:
+ 
+ expected worst-case time complexity is O(N);
+ expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
+ Elements of input arrays can be modified.
+ */
+
+public func solution3c(inout A : [Int]) -> Int {
+    let supposedSum = (A.count+1) * (A.count+2)/2
+    let sum = A.reduce(0, combine: +)
+    return supposedSum - sum
+}
+
+print(solution3c(&A))
 
